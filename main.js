53906863b3ccf7c93716993657b52ba737b16b0c
taishiasami123 <taishi.asami123@gmail.com>
2020-03-10 13:06:38 +0900

    指摘対応：フィールドから数値を取得する手法を修正

--- main.js
+++ main.js
@@ -3,31 +3,33 @@ const sub = document.getElementById("btn-sub");
 const mul = document.getElementById("btn-mul");
 const div = document.getElementById("btn-div");
 const result = document.getElementById("result");
+const input1 = document.getElementById("num1");
+const input2 = document.getElementById("num2");
 
 add.onclick = () => {
-  const num1 = parseFloat(document.getElementById("num1").value);
-  const num2 = parseFloat(document.getElementById("num2").value);
   calResult = num1 + num2;
+  const num1 = parseFloat(input1.value);
+  const num2 = parseFloat(input2.value);
   result.innerHTML = calResult;
 }
 
 sub.onclick = () => {
-  const num1 = parseFloat(document.getElementById("num1").value);
-  const num2 = parseFloat(document.getElementById("num2").value);
   calResult = num1 - num2;
+  const num1 = parseFloat(input1.value);
+  const num2 = parseFloat(input2.value);
   result.innerHTML = calResult;
 }
 
 mul.onclick = () => {
-  const num1 = parseFloat(document.getElementById("num1").value);
-  const num2 = parseFloat(document.getElementById("num2").value);
   calResult = num1 * num2;
+  const num1 = parseFloat(input1.value);
+  const num2 = parseFloat(input2.value);
   result.innerHTML = calResult;
 }
 
 div.onclick = () => {
-  const num1 = parseFloat(document.getElementById("num1").value);
-  const num2 = parseFloat(document.getElementById("num2").value);
+  const num1 = parseFloat(input1.value);
+  const num2 = parseFloat(input2.value);
   if (num2 == 0) {
     result.innerHTML = "<span style='color: red'>0で割ることはできません！</span>"
   } else {
