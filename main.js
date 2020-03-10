f34c7f4413b35caf25a4587acd1bd57974fee67a
taishiasami123 <taishi.asami123@gmail.com>
2020-03-10 13:00:58 +0900

    指摘対応：const resultを各スコープから外し冒頭で宣言

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const add = document.getElementById("btn-add");
 const sub = document.getElementById("btn-sub");
 const mul = document.getElementById("btn-mul");
 const div = document.getElementById("btn-div");
+const result = document.getElementById("result");
 
 add.onclick = () => {
   const num1 = parseFloat(document.getElementById("num1").value);
   const num2 = parseFloat(document.getElementById("num2").value);
-  const result = document.getElementById("result");
   calResult = num1 + num2;
   result.innerHTML = calResult;
 }
@@ -14,7 +14,6 @@ add.onclick = () => {
 sub.onclick = () => {
   const num1 = parseFloat(document.getElementById("num1").value);
   const num2 = parseFloat(document.getElementById("num2").value);
-  const result = document.getElementById("result");
   calResult = num1 - num2;
   result.innerHTML = calResult;
 }
@@ -22,7 +21,6 @@ sub.onclick = () => {
 mul.onclick = () => {
   const num1 = parseFloat(document.getElementById("num1").value);
   const num2 = parseFloat(document.getElementById("num2").value);
-  const result = document.getElementById("result");
   calResult = num1 * num2;
   result.innerHTML = calResult;
 }
@@ -30,7 +28,6 @@ mul.onclick = () => {
 div.onclick = () => {
   const num1 = parseFloat(document.getElementById("num1").value);
   const num2 = parseFloat(document.getElementById("num2").value);
-  const result = document.getElementById("result");
   if (num2 == 0) {
     result.innerHTML = "<span style='color: red'>0で割ることはできません！</span>"
   } else {
